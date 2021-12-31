741a6625ec5f04f92cf68250e95433f6c40c935e
Ренат <syltan.100renrgen@gmail.com>
2021-12-31 18:33:33 +0300

    add player scripts

---
+++ Assets/Scripts/Player/Player.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] private int _health;
+
+    public void ApplayDamage(int damage)
+    {
+        _health -= damage;
+
+        if (_health <= 0)
+        {
+            Die();
+        }
+    }
+
+    public void Die()
+    {
+
+    }
+}
---
+++ Assets/Scripts/Player/PlayerMover.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMover : MonoBehaviour
+{
+    [SerializeField] private float _speed;
+    [SerializeField] private float _stepSize;
+    [SerializeField] private float _maxRight;
+    [SerializeField] private float _maxLeft;
+
+    private Vector3 _targetPosition;
+
+    private void Start()
+    {
+        _targetPosition = transform.position;
+    }
+
+    private void Update()
+    {
+        if (transform.position != _targetPosition)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, _targetPosition, _speed * Time.deltaTime);
+        }
+    }
+
+    public void TryMoveRight()
+    {
+        if (_targetPosition.x < _maxRight)
+        {
+            SetNextPosition(_stepSize);
+        }
+    }
+
+    public void TryMoveLeft()
+    {
+        if (_targetPosition.x > _maxLeft)
+        {
+            SetNextPosition(-_stepSize);
+        }
+    }
+
+    private void SetNextPosition(float stepSize)
+    {
+        _targetPosition = new Vector2(_targetPosition.x + stepSize, _targetPosition.y);
+    }
+}
